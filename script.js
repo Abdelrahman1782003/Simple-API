f655bd5cf3b069d5bc3c4c89d3ee79c915dbced1
Abdelrahman <abdelrahman.ahmed1782003@gmail.com>
2026-07-19 22:16:27 +0300

    stage3 : create with validations

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 let TASKS = [
   { id: 1, title: "Study backend.", done: true },
   { id: 2, title: "Create APIs.", done: true },
   { id: 3, title: "Create a REST API site.", done: false },
 ];
+
+app.use(bodyParser.json());
+app.post("/tasks", (req, res, next) => {
+  let newTask = req.body;
+  console.log(newTask);
+  if (newTask && newTask.title) {
+    let newId = TASKS[TASKS.length - 1].id + 1;
+    newTask.id = newId;
+    newTask.done = false;
+    TASKS.push(newTask);
+    res.status(201);
+    return res.json({ message: "Created!", task: newTask });
+  }
+  res.status(400);
+  return res.json({ message: "Invalid title." });
+});
 
 app.get("/tasks/:id", (req, res, next) => {
   const taskId = Number(req.params.id);
