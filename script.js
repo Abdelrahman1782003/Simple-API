2aa221ca15b1038d6e9434690431012dc7d919c4
Abdelrahman <abdelrahman.ahmed1782003@gmail.com>
2026-07-19 21:39:28 +0300

    stage1: root and health endpoints

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/health", (req, res, next) => {
+  return res.json({ status: "ok" });
+});
+
 app.get("/", (req, res, next) => {
-  return res.json({ msg: "hello!!" });
+  return res.json({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
 
 app.listen(3000);
